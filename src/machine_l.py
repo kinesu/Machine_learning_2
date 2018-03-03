67b528bff0ebf8476a00e58240f4330da2013cf2
Kinga Erdanowska <kinesu@MacBook-Air-Kinga.local>
2018-03-03 15:38:39 +0100

    machine learning - adults database

---
+++ src/machine_l.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+#załadowanie danych
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
+names = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'capita-gain', 'capital-loss', 'hours-per-week', 'native-country']
+dataset = pd.read_csv(url, names=names)
+
+# shape
+print(dataset.shape)
+
+# head
+print(dataset.head(20))
+
+# descriptions
+print(dataset.describe())
+
+# grouping
+print(dataset.groupby('education').size())
+
+
+
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(7,7), sharex=False, sharey=False)
+plt.show()
+
+# histograms
+dataset.hist()
+plt.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+
+
+
+# Test options and evaluation metric
+seed = 7
+scoring = 'accuracy'
+
+
